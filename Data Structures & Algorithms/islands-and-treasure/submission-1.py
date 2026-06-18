1a7a9804c672fbe5a78711db048cf0cfe31fe32e
Ritesh Dubey <83487783+0xchikku@users.noreply.github.com>
2026-06-18 15:16:47 +0530

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    # time - O(n.m), space - O(n.m)
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        water = -1
+        treasure = 0
+        land = 2147483647
+        rows = len(grid)
+        cols = len(grid[0])
+        directions = [[-1,0],[1,0],[0,-1],[0,1]]
+        queue = deque([])
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == treasure:
+                    queue.append((row, col))
+        
+        while queue:
+            row, col = queue.popleft()
+            for dr, dc in directions:
+                mr, mc = dr + row, dc + col
+                if (mr < 0 or mc < 0 or mr >= rows or mc >= cols or grid[mr][mc] != land):
+                    continue
+                grid[mr][mc] = grid[row][col] + 1
+                queue.append((mr, mc))
+
+
