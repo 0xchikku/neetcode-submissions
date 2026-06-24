7f7c82c310e9ef32a8c355219b1cec15a9c844c4
Ritesh Dubey <83487783+0xchikku@users.noreply.github.com>
2026-06-24 17:53:31 +0530

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.py
@@ -0,0 +1,45 @@
+class Solution:
+    # time - O(n.m), space - O(n.m)
+    def numIslands(self, grid: List[List[str]]) -> int:
+        land = '1'
+        water = '0'
+        rows = len(grid)
+        cols = len(grid[0])
+        directions = [[-1,0],[1,0],[0,-1],[0,1]]
+        count = 0
+
+        def dfs(row, col):
+            grid[row][col] = water
+            for dr, dc in directions:
+                mr = dr + row
+                mc = dc + col
+                if (mr < 0 or mc < 0 or mr >= rows or mc >= cols or grid[mr][mc] != land):
+                    continue
+                dfs(mr, mc)
+                
+        def bfs(row, col):
+            grid[row][col] = water
+            queue = deque([(row, col)])
+            while queue:
+                row, col = queue.popleft()
+                for dr, dc in directions:
+                    mr = row + dr
+                    mc = col + dc
+                    if mr < 0 or mc < 0 or mr >= rows or mc >= cols or grid[mr][mc] != land:
+                        continue
+                    grid[mr][mc] = water
+                    queue.append((mr, mc))
+
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == land:
+                    # dfs(row, col)
+                    bfs(row, col)
+                    count += 1
+        
+        return count
+
+
+
+
